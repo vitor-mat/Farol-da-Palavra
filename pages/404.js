fbe5013d1c3be0c11eff87fd3e8029e6f78a13de
vitor-mat <vitormateusalves123@gmail.com>
2022-01-19 15:41:19 -0300

    pagina 404 criada

---
+++ pages/404.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+import styles from '../styles/Resume.module.css'
+  
+  
+const NotFound = ({ resume }) => {
+  
+    return (
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <Link href="/"><a><h1 className={styles.headerTitleText}>Farol da Palavra</h1></a></Link>
+          <Link href="/sobre"><a className={styles.linkSobreHeader}>Sobre</a></Link>
+        </header>
+        <main className={styles.main}>
+            <span className={styles.ErrorMessage}>ERROR: ESTA PÁGINA NÃO EXISTE.</span>
+            <Link href="/"><a className={styles.btnVolar}>Home</a></Link>
+        </main>
+        <footer className={styles.footer}>
+          <span className={styles.footerText}>Made by <a href="https://vitor-mat.github.io/portifolio/" target="_blank" rel="noreferrer">Vitor M.</a></span>
+        </footer>
+      </div>
+    )
+  }
+  
+export default NotFound
